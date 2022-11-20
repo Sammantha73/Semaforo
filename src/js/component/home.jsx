19018d7921d73bff79e218e5679bef3150d03bbc
Sammantha73 <Samanthafeijoom@gmail.com>
2022-11-20 04:37:53 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const ColorDefault ={red:"gray", yellow:"gray", green:"gray", rebeccapurple:"gray"};
+	const [ colors, SetColor] = useState(ColorDefault);
+	 const [showPurpura, setshowPurpura] = useState(true);
+
+	const ChangeColors =(ncolor)=>{
+		const color = colors [ncolor] == ncolor ? 'gray': ncolor;
+		const mycolors = {...colors};
+		mycolors[ncolor]=color;
+		SetColor(mycolors);
+	}
+
 	return (
+		<>
+		  
+
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+
+			<div id="ballWrapper">
+				{/*Con un estado adicional le indico cuando mostrarse*/}
+				<button onClick={() => setshowPurpura(!showPurpura)}>
+					{/*Aqui solo cambio el texto de mi boton */}
+					{showPurpura ? `Ocultar semaforo` : `Color Extra `}
+				</button>
+				<div className="ball" onClick={()=>ChangeColors('red')}  style={{backgroundColor: colors["red"]}}></div>
+				<div  className=" ball" onClick={()=>ChangeColors('yellow')}  style={{backgroundColor: colors["yellow"]}}></div>
+				<div  className="ball " onClick={()=>ChangeColors('green')}  style={{backgroundColor: colors["green"]}}></div>
+				<div className={showPurpura ? "show-element" : null}  >
+		 			 {showPurpura && <div  className=" ball " onClick={()=>ChangeColors('rebeccapurple')}  style={{backgroundColor: colors["rebeccapurple"]}}></div>}
+					 
+				</div>
+							
+			</div>	
 		</div>
+		</>
 	);
 };
 
 export default Home;
+
+
+
